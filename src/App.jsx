aa865c768e2d9ea2cb571a2c96ff04c0ac236fe9
Olagoke Olorunkosebi <olagokeolorunkosebi@gmail.com>
2025-03-15 13:38:26 +0100

    feat: add DeliveringToPage and profile icon for delivery details

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./pages/WelcomePage.jsx";
 import WelcomePage from "./pages/WelcomePage.jsx";
 import InstantDeliveryPage from "./pages/InstantDeliveryPage.jsx";
+import PickUpDetailsPage from "./pages/PickUpDetailsPage.jsx";
+import DeliveringToPage from "./pages/DeliveringToPage.jsx";
 import "./App.css";
 import "./index.css";
 
@@ -11,6 +13,8 @@ function App() {
       <div className="container">
         <WelcomePage />
         <InstantDeliveryPage />
+        <PickUpDetailsPage />
+        <DeliveringToPage />
       </div>
     </>
   );
---
+++ src/pages/PickUpDetailsPage.jsx
@@ -0,0 +1,198 @@
+import React from "react";
+import Caret from "../assets/images/caret-back.png";
+import ProfileIcon from "../assets/images/profile-icon.png";
+
+const PickUpDetailsPage = () => {
+  return (
+    <div
+      className="profile-details-page"
+      style={{
+        backgroundColor: "#FFFFFF",
+        boxShadow: "0px 0px 4px 4px rgba(0, 0, 0, 0.25)",
+        paddingTop: "10px",
+        // maxWidth: "350px",
+        width: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <header
+        className="header"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          padding: "0 20px ",
+          maxWidth: "100vw",
+          marginBottom: "-10px",
+        }}
+      >
+        <div
+          className="header-content"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <div>
+            <img
+              src={Caret}
+              alt="Back Button"
+              style={{ marginRight: "15px", cursor: "pointer" }}
+            />
+          </div>
+          <div
+            className="header-dash"
+            style={{
+              width: "35px",
+              height: "5px",
+              backgroundColor: "#9D0208",
+              borderRadius: "5px",
+            }}
+          ></div>
+          <div>
+            <img
+              src={ProfileIcon}
+              alt="Back Button"
+              style={{ marginRight: "15px", cursor: "pointer" }}
+            />
+          </div>
+        </div>
+      </header>
+      <div className="instant-delivery-content" style={{ padding: "20px" }}>
+        <p
+          className="form-title"
+          style={{ fontWeight: "500", marginBottom: "8px" }}
+        >
+          Pickup Details
+        </p>
+
+        {/* form creation */}
+        <form id="pickup-details-form">
+          <div className="form-group">
+            <label
+              htmlFor="pickup-details-name"
+              style={{ fontSize: "14px", marginBottom: "10px" }}
+            >
+              Name (shop, person)
+            </label>
+            <br />{" "}
+            <input
+              id="pickup-details-name"
+              placeholder="Feedwell, Akobo"
+              style={{
+                width: "100%",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                padding: " 10px ",
+                boxSizing: "border-box",
+                fontSize: "12px",
+              }}
+            />
+          </div>
+          <br />
+          <div className="form-group">
+            <label
+              htmlFor="pickup-details-phone"
+              style={{ fontSize: "14px", marginBottom: "10px" }}
+            >
+              Phone Number
+            </label>
+            <br />{" "}
+            <input
+              id="pickup-details-phone"
+              placeholder="08123456789"
+              style={{
+                width: "100%",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                padding: " 10px ",
+                boxSizing: "border-box",
+                fontSize: "12px",
+              }}
+            />
+          </div>
+          <br />
+          <div className="form-group">
+            <label
+              htmlFor="pickup-details-building-number"
+              style={{ fontSize: "14px", marginBottom: "10px" }}
+            >
+              Building/Floor Number
+            </label>
+            <br />{" "}
+            <input
+              id="pickup-details-building-number"
+              placeholder="No 1"
+              style={{
+                width: "100%",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                padding: " 10px",
+                boxSizing: "border-box",
+                fontSize: "12px",
+              }}
+            />
+          </div>
+          <br />
+          <div className="form-group">
+            <label
+              htmlFor="pickup-details-busstop"
+              style={{ fontSize: "14px", marginBottom: "10px" }}
+            >
+              Landmark/Busstop
+            </label>
+            <br />{" "}
+            <input
+              id="pickup-details-busstop"
+              placeholder="Faith Bible Church"
+              style={{
+                width: "100%",
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                padding: " 10px",
+                boxSizing: "border-box",
+                fontSize: "12px",
+              }}
+            />
+          </div>
+          <br />
+          <div
+            className="buttons"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: "20px",
+              boxSizing: "border-box",
+              gap: "10px",
+            }}
+          >
+            <button
+              style={{
+                backgroundColor: "#fff",
+                border: "1px solid #9D0208",
+                fontWeight: "600",
+                width: "50%",
+              }}
+            >
+              Back
+            </button>
+            <button
+              style={{
+                backgroundColor: "#9D0208",
+                border: "1px solid #9D0208",
+                color: "#fff",
+                fontWeight: "600",
+                width: "50%",
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PickUpDetailsPage;
